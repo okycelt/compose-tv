4d1b75641c4c23a01a7ba3fae42fdbeea266fb80
Ondřej Kycelt <26062845+okycelt@users.noreply.github.com>
2023-03-29 19:54:10 +0200

    Startup benchmark for different compilation modes

--- benchmark/src/main/java/cz/okycelt/composetv/benchmark/StartupBenchmark.kt
+++ benchmark/src/main/java/cz/okycelt/composetv/benchmark/StartupBenchmark.kt
@@ -1,5 +1,6 @@
 package cz.okycelt.composetv.benchmark
 
+import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
@@ -16,11 +17,17 @@ class StartupBenchmark {
     val benchmarkRule = MacrobenchmarkRule()
 
     @Test
-    fun startup() = benchmarkRule.measureRepeated(
+    fun startupCompilationNone() = startup(CompilationMode.None())
+
+    @Test
+    fun startupCompilationPartial() = startup(CompilationMode.Partial())
+
+    private fun startup(compilationMode: CompilationMode) = benchmarkRule.measureRepeated(
         packageName = "cz.okycelt.composetv",
         metrics = listOf(StartupTimingMetric()),
         iterations = 5,
-        startupMode = StartupMode.COLD
+        startupMode = StartupMode.COLD,
+        compilationMode = compilationMode
     ) {
         pressHome()
         startActivityAndWait()
